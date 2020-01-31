4b064bbde051aa6c69cf5fcf8ea8f545fb3df5db
Long <tqlong@gmail.com>
2020-01-31 14:37:04 +0700

    add release build

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-    shared_ptr<Game> game = make_shared<PingPongGame>("Collision System", 1000, 3.3f);
+    shared_ptr<Game> game = make_shared<PingPongGame>("Collision System", 2000, 3.f);
     game->createGame(game);
     game->loop();
     return 0;
