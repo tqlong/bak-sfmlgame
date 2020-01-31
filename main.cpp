31f5f0189e94e346fd72e92c2c7b8682a9b6ed4b
Long <tqlong@gmail.com>
2020-01-31 14:25:25 +0700

    best demo parameter

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-    shared_ptr<Game> game = make_shared<PingPongGame>("Collision System", 200, 2.f);
+    shared_ptr<Game> game = make_shared<PingPongGame>("Collision System", 1000, 3.3f);
     game->createGame(game);
     game->loop();
     return 0;
